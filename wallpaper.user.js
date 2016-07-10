f55eafefba14a011b68e101172417c6a44c58a7b
krishnachaitanya9 <chaitanyaradon89@gmail.com>
2016-07-10 12:35:43 +0530

    Update wallpaper.user.js

--- wallpaper.user.js
+++ wallpaper.user.js
@@ -11,13 +11,13 @@ var lastitem = urlarray.pop();
 number = parseInt(lastitem);
 number += 1;
 lastitem = number.toString();
-return lastitem}
+return lastitem;}
 function do_next_activity(){
 q = window.location.href;
 q = q.substring(0, q.length - 1);
 if ( next_page_number() > 9){q = q.substring(0, q.length - 1);}
 final_url = q.concat(next_page_number());
-window.open(final_url,"_self")
+window.open(final_url,"_self");
 }
 function previous_page_number(){
 k = window.location.href;
@@ -32,18 +32,12 @@ q = window.location.href;
 q = q.substring(0, q.length - 1);
 if ( parseInt(previous_page_number()) >= 9){q = q.substring(0, q.length - 1);}
 final_url = q.concat(previous_page_number());
-window.open(final_url,"_self")
+window.open(final_url,"_self");
 }
 
 window.addEventListener("keydown", checkKeyPressed, false);
  
 function checkKeyPressed(e) {
-    if (e.keyCode == "39") {
-       
-       do_next_activity();
-    }
-    if (e.keyCode == "37") {
-        
-       do_previous_activity();
-    }
+    if (e.keyCode == "39") { do_next_activity();}
+    if (e.keyCode == "37") { do_previous_activity();}
 }
